b5777df918c0d56409a2458ea4fdf81092576a1f
Шапошников Александр Владимирович <Shaposhnikov@cspmz.ru>
2020-09-17 19:58:08 +0300

    added data of last infected count to calculate predicted values from pct_change

--- all_regions.py
+++ all_regions.py
@@ -11,7 +11,7 @@ from matplotlib import pyplot as plt
 from sklearn.metrics import mean_squared_error as mse
 from sklearn.metrics import r2_score
 from scipy.optimize import curve_fit
-from sklearn.linear_model import LinearRegression 
+from sklearn.linear_model import LinearRegression
 
 PREDICTOR_DAYS = 20  # количество дней в качестве предикторов
 PREDICTED_DAYS = [1, 5, 10, 15, 30]  # на какое количество дней вперед предсказывать данные
@@ -35,14 +35,17 @@ def transform_data(df: pd.DataFrame):
 
 def make_learning_data(df: 'pd.DataFrame',
                        predictor_days: int,
-                       predicted_days: 'iterable') -> 'tuple of DataFrames':
+                       predicted_days: 'iterable',
+                       test_data=False) -> 'tuple of DataFrames':
     """
     Описание ...
-    y_dates
+    Если test_data == True, то возвращаются дополнительные данные,
+    иначе - пустые списки
     """
     x_data = []
     y_data = []
     y_dates = []
+    x_last_infected_data = []
     single_dataset_size = predictor_days + max(predicted_days)
     for i in range(len(df) - single_dataset_size):
         # проверка качества датасетов, плохие не включаем
@@ -61,15 +64,18 @@ def make_learning_data(df: 'pd.DataFrame',
         y_pct_change = {key: [(val - x_last_infected) / x_last_infected] for key, val in y_infected.items()}
         y_data.append(pd.DataFrame(y_pct_change))
         # Соберем даты для точек зависимой переменной
-        y_date_values = {str(day): [df.iloc[i + predictor_days - 1 + day].name] for day in predicted_days}
-        y_dates.append(pd.DataFrame(y_date_values))
+        if test_data:
+            y_date_values = {str(day): [df.iloc[i + predictor_days - 1 + day].name] for day in predicted_days}
+            y_dates.append(pd.DataFrame(y_date_values))
+            x_last_infected_data.append(x_last_infected)
     if x_data:  # возвращаем результат только если он есть
         x_data = pd.concat(x_data, ignore_index=True)
         y_data = pd.concat(y_data, ignore_index=True)
-        y_dates = pd.concat(y_dates, ignore_index=True)
-        return x_data, y_data, y_dates
+        if test_data:
+            y_dates = pd.concat(y_dates, ignore_index=True)
+        return x_data, y_data, y_dates, x_last_infected_data
     else:
-        return None, None, None
+        return (None,) * 4
 
 
 source_folder = 'data/source'
@@ -83,18 +89,23 @@ train_x = []
 train_y = []
 test_x = []
 test_y = []
-test_y_dates = []  # ДАЛЕЕ ПОДПРАВИТЬ, ПЖЛСТ
+test_y_dates = []
+test_x_last_infected = []
 for region, region_frame in frame.groupby('Region/City'):
     transformed = transform_data(region_frame)
-    x, y, dates = make_learning_data(transformed, PREDICTOR_DAYS, PREDICTED_DAYS)
-    if x is not None:
-        if region != PREDICTED_REGION:
+    if region != PREDICTED_REGION:
+        x, y, *trash = make_learning_data(transformed, PREDICTOR_DAYS, PREDICTED_DAYS)
+        if x is not None:
             train_x.append(x)
             train_y.append(y)
-        else:
+    else:
+        x, y, dates, x_last_infected = make_learning_data(transformed, PREDICTOR_DAYS, PREDICTED_DAYS,
+                                                          test_data=True)
+        if x is not None:
             test_x.append(x)
             test_y.append(y)
             test_y_dates.append(dates)
+            test_x_last_infected.append(x_last_infected)
 
 train_x = pd.concat(train_x, ignore_index=True)
 train_y = pd.concat(train_y, ignore_index=True)
